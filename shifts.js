9bd72e0990cdb2774891a91618316474b53bc617
Jaylord Mantuhac <jaylordmantuhacp@gmail.com>
2026-09-21 22:41:15 +0800

    Add overtime pay for shifts over 8 hours - mantuhac.jaylord

--- shifts.js
+++ shifts.js
@@ -1,9 +1,9 @@
-function isValidShift(hours) {
-  return hours > 0 && hours <= 24;
-}
-
 function calculatePay(hours, rate) {
-  return Math.floor(hours * rate);
-}
+  const regularHours = Math.min(hours, 8);
+  const overtimeHours = Math.max(hours - 8, 0);
 
-module.exports = { isValidShift, calculatePay };
+  const regularPay = regularHours * rate;
+  const overtimePay = overtimeHours * rate * 1.5;
+
+  return Math.floor(regularPay + overtimePay);
+}
